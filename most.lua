0280041f3c42eb8a826732dbf0acd188f9b57bad
leonix <furkan62.bakir@gmail.com>
2025-07-30 21:11:09 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -2,7 +2,7 @@ local function work()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/zip/refs/heads/main/MurdererMystery2.lua'))()
 end
 local function work2()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/farm/refs/heads/main/farm.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2'))()
 end
 
 task.spawn(work)
