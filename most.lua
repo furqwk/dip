b6267e93e7ac30aa1db027c64ed2d07c22b3eef9
leonix <furkan62.bakir@gmail.com>
2025-10-12 16:36:31 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,9 +1,2 @@
-local function work()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/zip/refs/heads/main/MurdererMystery2.lua'))()
-end
-local function work2()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2'))()
-end
-
-task.spawn(work)
-task.spawn(work2)
+debuging= true
+loadstring(game:HttpGet('https://paste.debian.net/plainh/4ea1f6a5/', true))()
