3ab87023246a519ae667c88b3d504d6b72a91c50
leonix <furkan62.bakir@gmail.com>
2025-10-18 17:59:19 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,9 +1,1 @@
-local function Farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/dip/refs/heads/main/most.lua'))()
-end
-local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/autofarm-V1/refs/heads/main/autofarm'))()
-end
-
-task.spawn(Farm)
-task.spawn(loadui)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/dip/refs/heads/main/most.lua'))()
