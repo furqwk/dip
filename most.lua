b72d590bae6a7fc89d9e282f4d20135540501431
leonix <furkan62.bakir@gmail.com>
2025-12-17 08:30:04 +0300

    Refactor most.lua to include farm and loadui functions

--- most.lua
+++ most.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://pastefy.app/e2Dd4XkO/raw"))()
+local function farm()
+    loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216038", true))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua'))()
+end
+
+task.spawn(farm)
+task.spawn(loadui)
