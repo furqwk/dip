06272fe8e9dbdbbf578210efdb2b0e8fefef821b
leonix <furkan62.bakir@gmail.com>
2025-12-21 04:21:11 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,5 +1,5 @@
 local function farm()
-    loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216724", true))()
+    loadstring(game:HttpGet("http://vpaste.net/J6u0D", true))()
 end
 local function loadui()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua'))()
