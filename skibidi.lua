dd183523882fc0a173e367ff00a627301d207eb4
leonix <furkan62.bakir@gmail.com>
2026-01-28 05:14:32 +0300

    Create skibidi.lua

---
+++ skibidi.lua
@@ -0,0 +1,5 @@
+if identifyexecutor and identifyexecutor() == "Delta" then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/bra/refs/heads/main/fyp.lua'))()
+else
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/dip/refs/heads/main/alt'))()
+end
