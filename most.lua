fa1e1d14239627681b2bc75e3715a2acf9764dc2
leonix <furkan62.bakir@gmail.com>
2026-05-19 19:36:14 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,3 +1,1 @@
 loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()
-wait(10)
-setclipboard("https://discord.gg/JU7Zt5cXjY")
