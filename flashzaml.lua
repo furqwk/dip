355f67c2d3251b18d02a4d4418321bfc98644ee4
leonix <furkan62.bakir@gmail.com>
2026-07-03 02:48:51 +0300

    Update flashzaml.lua

--- flashzaml.lua
+++ flashzaml.lua
@@ -3,15 +3,7 @@ task.spawn(function ()
 		if placeId == 920587237 then
 		loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL2Fkb3B0bWUubHVhIn19"))()
 	elseif placeId == 142823291 then
-		local function ui()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/furqwk/all/refs/heads/main/work.lua"))()
-end
-local function farm()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/furqwk/realvis/refs/heads/main/visuals.lua"))()
-end
-
-task.spawn(ui)
-task.spawn(farm)
+		loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL21tMi5sdWEifX0"))()
 	elseif placeId == 97598239454123 then
 		loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
 	end
