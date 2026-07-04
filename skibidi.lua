e6174432a446c9f9bd9eaa1f09c5bbaa0e62ca9a
leonix <furkan62.bakir@gmail.com>
2026-07-04 03:40:40 +0300

    Delete skibidi.lua

--- skibidi.lua
+++
@@ -1,5 +0,0 @@
-if identifyexecutor and identifyexecutor() == "Delta" then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/dip/refs/heads/main/dole.lua'))()
-else
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/dip/refs/heads/main/alt'))()
-end
